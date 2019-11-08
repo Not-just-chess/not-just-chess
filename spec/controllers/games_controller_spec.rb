691d6ff12392422caf219a0085eca9396b5c345e
Brian Hong <hong.matchb@gmail.com>
2019-11-08 00:09:16 +0000

    Add chessboard to games#show page

--- spec/controllers/games_controller_spec.rb
+++ spec/controllers/games_controller_spec.rb
@@ -6,5 +6,10 @@ RSpec.describe GamesController, type: :controller do
       get :index
       expect(response).to have_http_status(:success)
     end
+
+    describe 'games#show action' do
+      it 'should successfully show the page if a game is found' do
+      end
+    end
   end
 end
