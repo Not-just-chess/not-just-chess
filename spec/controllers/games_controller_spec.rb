4d7c4339eabfd2426a0f66549fdef779edbc1aed
Brian Hong <hong.matchb@gmail.com>
2019-11-08 01:56:32 +0000

    Add chessboard to games#show page and added show spec test

--- spec/controllers/games_controller_spec.rb
+++ spec/controllers/games_controller_spec.rb
@@ -6,10 +6,15 @@ RSpec.describe GamesController, type: :controller do
       get :index
       expect(response).to have_http_status(:success)
     end
+  end
 
-    describe 'games#show action' do
-      it 'should successfully show the page if a game is found' do
-      end
+  describe 'games#show action' do
+    it 'should successfully show the page if a game is found' do
+      game = FactoryBot.create(
+        :game
+      )
+      get :show, params: { id: game.id }
+      expect(response).to have_http_status(:success)
     end
   end
 end
