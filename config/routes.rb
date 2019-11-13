f0a2f8ee74b59833043c871e333d09c3a4c9e2dc
Bre Phelps <bphelps@live.com>
2019-11-13 21:42:39 +0000

    Routes changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'static_page#index'
-  resources :games, only: [:new, :index, :show, :create, :update]
+  root 'games#index'
+  resources :games, only: [:new, :show, :create, :update]
   resources :chess_pieces, only: [:new, :create]
+  resources :static_page, only: [:index]
 end
