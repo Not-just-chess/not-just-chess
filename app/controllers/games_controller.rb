33c33faed01383e810c8e60ee9d418df124e78ea
Bre Phelps <bphelps@live.com>
2019-11-13 22:00:18 +0000

    changed color to boolean

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -8,6 +8,7 @@ class GamesController < ApplicationController
 
   def create
     @game = current_user.games.create(game_params)
+    populate_game
     redirect_to root_path
   end
 
--- app/models/game.rb
+++ app/models/game.rb
@@ -3,4 +3,10 @@ class Game < ApplicationRecord
   belongs_to :user
   delegate :kings, :queens, :knights, :rooks, :bishops, :pawns, to: :chess_pieces
   scope :available, -> { where(black_player_id: nil).or.where(white_player_id: nil) }
+
+  def populate_game
+    # first need to initialize the chess pieces
+    # then need to place pieces according to board setup
+    # lastly need to either default color by placement on board, or have separate method called to do so
+  end
 end
