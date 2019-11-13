f60e3ebe51b138a725640ef17aca2897129c329c
Bre Phelps <bphelps@live.com>
2019-11-13 21:19:55 +0000

    moved is_obstructed? method from Game model to ChessPiece model

--- app/models/chess_piece.rb
+++ app/models/chess_piece.rb
@@ -12,4 +12,26 @@ class ChessPiece < ApplicationRecord
   scope :knights, -> { where(type: 'Knight') }
   scope :bishops, -> { where(type: 'Bishop') }
   scope :pawns, -> { where(type: 'Pawn') }
+
+  def is_obstructed(current_location, destination)
+    x1 = current_location[0]
+    y1 = current_location[1]
+    x2 = destination[0]
+    y2 = destination[1]
+
+    x_delta = x2 - x1
+    y_delta = y2 - y1
+    x_dir = x_delta / x_delta.abs
+    y_dir = y_delta / y_delta.abs
+
+    x_move = x1
+    y_move = y1
+
+    while x_move != x2 || y_move != y2
+      x_move = x1 + x_dir
+      y_move = y1 + y_dir
+
+      return true if @rows[x_move][y_move]
+    end
+  end
 end
--- app/models/game.rb
+++ app/models/game.rb
@@ -1,29 +1,6 @@
-# Game model
 class Game < ApplicationRecord
   has_many :chess_pieces
   belongs_to :user
   delegate :kings, :queens, :knights, :rooks, :bishops, :pawns, to: :chess_pieces
   scope :available, -> { where(black_player_id: nil).or.where(white_player_id: nil) }
-
-  def is_obstructed(current_location, destination)
-    x1 = current_location[0]
-    y1 = current_location[1]
-    x2 = destination[0]
-    y2 = destination[1]
-
-    x_delta = x2 - x1
-    y_delta = y2 - y1
-    x_dir = x_delta / x_delta.abs
-    y_dir = y_delta / y_delta.abs
-
-    x_move = x1
-    y_move = y1
-
-    while x_move != x2 || y_move != y2
-      x_move = x1 + x_dir
-      y_move = y1 + y_dir
-
-      return true if @rows[x_move][y_move]
-    end
-  end
 end
