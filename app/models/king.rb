3a3da1c62701898869a6aeb201491104626112d3
Robert Currie <currierobertm@gmail.com>
2019-11-13 23:09:53 +0000

    Fixed valid_move? for Travis_CI

--- app/models/king.rb
+++ app/models/king.rb
@@ -1,36 +1,36 @@
 # King subclass
 class King < ChessPiece
 
-	def valid_move?(current_location, destination)
-		@moves = [
-			[1, 1],
-			[1, 0],
-			[1, -1],
-			[0, 1],
-			[0, -1],
-			[1, 1],
-			[1, 0],
-			[1, -1]
-		]
+  def valid_move?(current_location, destination)
+    @moves = [
+      [1, 1],
+      [1, 0],
+      [1, -1],
+      [0, 1],
+      [0, -1],
+      [1, 1],
+      [1, 0],
+      [1, -1]
+    ]
 
-		x1 = current_location[0]
+    x1 = current_location[0]
     y1 = current_location[1]
     x2 = destination[0]
-		y2 = destination[1]
-		
-		@moves.each do |move|
-			move[0] += x1
-			move[1] += y1
-		end
+    y2 = destination[1]
+    
+    @moves.each do |move|
+      move[0] += x1
+      move[1] += y1
+    end
 
-		if x2 < 0 || x2 > 7 || y2 < 0 || y2 > 7
-			return false
-		end
+    if x2 < 0 || x2 > 7 || y2 < 0 || y2 > 7
+      return false
+    end
 
-		if @moves.include? destination && @rows[x2, y2] == false
-			return true
-		end
+    if @moves.include?(destination) && @rows[x2, y2] == false
+      return true
+    end
 
-	end
+  end
 
 end
