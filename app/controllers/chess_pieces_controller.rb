d3c0601635fa1e3f98cc7b543858d2d63e672ee0
Brian Hong <hong.matchb@gmail.com>
2019-11-15 19:32:19 +0000

    Added Pieces unicode

--- app/controllers/chess_pieces_controller.rb
+++ app/controllers/chess_pieces_controller.rb
@@ -6,6 +6,12 @@ class ChessPiecesController < ApplicationController
     ChessPiece.create(chess_piece_params)
   end
 
+  def show
+    @chess_piece = ChessPiece.find_by_id(params[:id])
+    @game = @chess_piece.game
+    @chess_pieces = @game.chess_pieces
+  end
+
   def chess_piece_params
     params.require(:chess_piece).permit(:type, :game_id)
   end
