6db9cdfc222bb0663dabb30643f9ebe01cf07e04
Bre Phelps <bphelps@live.com>
2019-11-15 16:45:55 +0000

    refactoring code

--- app/helpers/games_helper.rb
+++ app/helpers/games_helper.rb
@@ -1,14 +1,8 @@
 # Games Helper
 module GamesHelper
-  attr_accessor :x, :y, :type, :pieces
+  attr_accessor :pieces
   
   def render_piece(game)
     @pieces = ChessPiece.where(game_id: game.id)
-
-    @pieces.each do |piece|
-      @x = piece.x_position
-      @y = piece.y_position
-      @type = piece.type
-    end
   end
 end
