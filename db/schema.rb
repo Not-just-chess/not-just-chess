5a3da3ca8aa905e4610f04329ea3911a7e30ae78
Bre Phelps <bphelps@live.com>
2019-11-18 21:01:03 +0000

    removed extra test on chesspieces controller

--- db/schema.rb
+++ db/schema.rb
@@ -10,75 +10,77 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_191_118_001_935) do
+ActiveRecord::Schema.define(version: 2019_11_18_001935) do
+
   # These are extensions that must be enabled in order to support this database
-  enable_extension 'plpgsql'
+  enable_extension "plpgsql"
 
-  create_table 'bishops', force: :cascade do |t|
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
+  create_table "bishops", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  create_table 'chess_pieces', force: :cascade do |t|
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.string 'type'
-    t.integer 'game_id'
-    t.boolean 'color'
-    t.boolean 'captured'
-    t.integer 'x_position'
-    t.integer 'y_position'
-    t.string 'htmlcode'
-    t.index ['game_id'], name: 'index_chess_pieces_on_game_id'
+  create_table "chess_pieces", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "type"
+    t.integer "game_id"
+    t.boolean "color"
+    t.boolean "captured"
+    t.integer "x_position"
+    t.integer "y_position"
+    t.string "htmlcode"
+    t.index ["game_id"], name: "index_chess_pieces_on_game_id"
   end
 
-  create_table 'games', force: :cascade do |t|
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.integer 'user_id'
-    t.integer 'white_player_id'
-    t.integer 'black_player_id'
-    t.index ['black_player_id'], name: 'index_games_on_black_player_id'
-    t.index ['user_id'], name: 'index_games_on_user_id'
-    t.index ['white_player_id'], name: 'index_games_on_white_player_id'
+  create_table "games", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "user_id"
+    t.integer "white_player_id"
+    t.integer "black_player_id"
+    t.index ["black_player_id"], name: "index_games_on_black_player_id"
+    t.index ["user_id"], name: "index_games_on_user_id"
+    t.index ["white_player_id"], name: "index_games_on_white_player_id"
   end
 
-  create_table 'kings', force: :cascade do |t|
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
+  create_table "kings", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  create_table 'knights', force: :cascade do |t|
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
+  create_table "knights", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  create_table 'pawns', force: :cascade do |t|
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
+  create_table "pawns", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  create_table 'queens', force: :cascade do |t|
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
+  create_table "queens", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  create_table 'rooks', force: :cascade do |t|
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
+  create_table "rooks", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  create_table 'users', force: :cascade do |t|
-    t.string 'email', default: '', null: false
-    t.string 'encrypted_password', default: '', null: false
-    t.string 'reset_password_token'
-    t.datetime 'reset_password_sent_at'
-    t.datetime 'remember_created_at'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.string 'provider'
-    t.string 'uid'
-    t.index ['email'], name: 'index_users_on_email', unique: true
-    t.index ['reset_password_token'], name: 'index_users_on_reset_password_token', unique: true
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "provider"
+    t.string "uid"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
+
 end
--- spec/controllers/chess_pieces_controller_spec.rb
+++ spec/controllers/chess_pieces_controller_spec.rb
@@ -2,26 +2,6 @@
 require 'rails_helper'
 
 RSpec.describe ChessPiecesController, type: :controller do
-=begin
-  describe 'chess_pieces#create' do
-    it 'should allow new chess_pieces to be created' do
-      user = FactoryBot.create(:user)
-      sign_in user
-      game1 = FactoryBot.create(:game)
-
-      post :create, params: { chess_piece: {
-        game_id: game1[:id],
-        type: 'King'
-      } }
-      expect(response).to have_http_status(:success)
-
-      king = ChessPiece.last
-      expect(king.type).to eq('King')
-      expect(king.game_id).to eq(game1[:id])
-    end
-  end
-=end
-
   describe 'chess_pieces#update' do
     before :each do
       @g = FactoryBot.create(:game)
