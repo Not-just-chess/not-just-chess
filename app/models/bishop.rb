20f10314a636da754f46abd47a130850284c7c11
Bre Phelps <bphelps@live.com>
2019-11-20 16:29:48 +0000

    update code with new version of is_obstructed

--- app/models/bishop.rb
+++ app/models/bishop.rb
@@ -1,2 +1,30 @@
 class Bishop < ChessPiece
+  def valid_move?(current_location, destination)
+    # Bishop can move any number of spaces
+    # as long as moves are diagonal
+
+    # current x_position and y_position
+    x1 = current_location[0]
+    y1 = current_location[1]
+
+    # destination_x and destination_y
+    x2 = destination[0]
+    y2 = destination[1]
+
+    # take delta/slope
+    x_delta = x2 - x1
+    y_delta = y2 - y1
+    slope = y_delta / x_delta
+
+    # abs value of slope should always equal 1
+    if slope.abs == 1
+      if is_obstructed?(current_location, destination)
+        false
+      else
+        true
+      end
+    else
+      false
+    end
+  end
 end
