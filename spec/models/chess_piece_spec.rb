96bf6e4929a62e8c88f07ef5b2164ba851a48d50
Bre Phelps <bphelps@live.com>
2019-11-24 21:22:58 +0000

    initial commit

--- spec/models/chess_piece_spec.rb
+++ spec/models/chess_piece_spec.rb
@@ -1,4 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe ChessPiece, type: :model do
+  describe 'is_obstructed?' do
+    it 'checks if bishop is obstructed during a move' do
+      game = FactoryBot.create(:game)
+      game.populate_game
+      bishop = Bishop.last
+      expect(bishop.x_position).to eq(6)
+      expect(bishop.y_position).to eq(8)
+
+      bishop.is_obstructed?([6, 8], [8, 6])
+      expect(false)
+    end
+  end
 end
