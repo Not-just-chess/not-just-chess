3fe9f27c539a9ad8f6648b61cc0ffb796146dfa4
Ivan Perez <ivan.perez002@gmail.com>
2019-11-25 00:53:50 +0000

    did rubocop fixes

--- app/models/queen.rb
+++ app/models/queen.rb
@@ -1,6 +1,5 @@
 # Queen subclass
 class Queen < ChessPiece
-
   def valid_move?(current_location, destination)
     x1 = current_location[0]
     y1 = current_location[1]
@@ -9,18 +8,18 @@ class Queen < ChessPiece
 
     y_delta = (y2 - y1)
     x_delta = (x2 - x1)
-    
-    if y_delta == 0 || x_delta == 0
-      slope = 0
-    else
-      slope = y_delta.abs / x_delta.abs
-    end
+
+    slope = if y_delta.zero? || x_delta.zero?
+              0
+            else
+              y_delta.abs / x_delta.abs
+            end
 
     # Check if the destination location is of off the board
     return false if off_board?(x2, y2)
 
     # Vertical Check || Horizontal Check || Diagonal Check
     return false unless y1 == y2 || x1 == x2 || slope == 1
-    return false if is_obstructed?([x1,y1], [x2,y2])
+    return false if is_obstructed?([x1, y1], [x2, y2])
   end
 end
--- spec/models/queen_spec.rb
+++ spec/models/queen_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Queen, type: :model do
-    describe 'valid_move?' do
+  describe 'valid_move?' do
     it 'verifies valid moves' do
       game = FactoryBot.create(:game)
       game.populate_game
@@ -12,7 +12,7 @@ RSpec.describe Queen, type: :model do
       piece.valid_move?([4, 8], [4, 4])
       expect(true)
 
-      #checking diagonal move
+      # checking diagonal move
       piece.valid_move?([4, 8], [8, 4])
       expect(true)
 
@@ -21,11 +21,11 @@ RSpec.describe Queen, type: :model do
       expect(false)
 
       # checking for a move of the board
-      piece.valid_move?([4,8], [9,9])
+      piece.valid_move?([4, 8], [9, 9])
       expect(false)
 
       # checking for a move that is obstructed
-      piece.valid_move?([4,8], [3,8])
+      piece.valid_move?([4, 8], [3, 8])
       expect(false)
     end
   end
