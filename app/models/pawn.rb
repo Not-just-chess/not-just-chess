795aff04c148b29e512f7f3478a7f8502afa66f5
Brian Hong <hong.matchb@gmail.com>
2019-11-25 08:38:59 +0000

    Initial Pawn valid_move? method

--- app/models/pawn.rb
+++ app/models/pawn.rb
@@ -1,6 +1,28 @@
 # Pawn subclass
 class Pawn < ChessPiece
-  # def piece_unicode
-  #  y_position < 5 ? '&#9817;' : '&#9823;'
-  # end
+  def valid_move?(current_location, destination)
+    x1 = current_location[0]
+    y1 = current_location[1]
+    x2 = destination[0]
+    y2 = destination[1]
+
+    return false if off_board?(x2, y2)
+
+    moves = []
+
+    # White
+    if color == true
+      moves.push([x1.to_i, y1.to_i + 1])
+      moves.push([x1.to_i - 1, y1.to_i + 1])
+      moves.push([x1.to_i + 1, y1.to_i + 1])
+      moves.push([x1.to_i, y1.to_i + 2]) if y1 == 2
+    # Black
+    elsif color == false
+      moves.push([x1.to_i, y1.to_i - 1])
+      moves.push([x1.to_i - 1, y1.to_i - 1])
+      moves.push([x1.to_i + 1, y1.to_i - 1])
+      moves.push([x1.to_i, y1.to_i - 2]) if y_position == 7
+    end
+    moves.include?([x2.to_i, y2.to_i])
+  end
 end
--- spec/models/pawn_spec.rb
+++ spec/models/pawn_spec.rb
@@ -1,4 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe Pawn, type: :model do
+  describe 'valid_move?' do
+    it 'verifies valid moves' do
+      game = FactoryBot.create(:game)
+      game.populate_game
+      piece = Pawn.last
+
+      # Move up 2 spaces on first move
+      piece.valid_move?([2, 2], [2, 4])
+      expect(true)
+
+      # Move 1 space vertically
+      piece.valid_move?([1, 1], [1, 2])
+      expect(true)
+
+      # checking for a non-diagonal/vertical/horizontal move
+      piece.valid_move?([4, 8], [5, 6])
+      expect(false)
+
+      # checking for a move of the board
+      piece.valid_move?([4, 8], [9, 9])
+      expect(false)
+    end
+  end
 end
