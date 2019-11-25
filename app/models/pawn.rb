d5c10c8cc4c1bb4841d08d9b1db1e039541a6011
Ivan Perez <ivan.perez002@gmail.com>
2019-11-25 06:22:56 +0000

    initial shot at checking for valid pawn move

--- app/models/pawn.rb
+++ app/models/pawn.rb
@@ -1,6 +1,44 @@
 # Pawn subclass
 class Pawn < ChessPiece
-  # def piece_unicode
-  #  y_position < 5 ? '&#9817;' : '&#9823;'
-  # end
+  # Can move only forward
+  # Move 1 at a time except on its first move
+  # Captures diagonally
+
+  def valid_move?(current_location, destination)
+    x1 = current_location[0]
+    y1 = current_location[1]
+    x2 = destination[0]
+    y2 = destination[1]
+
+    @moves_white = [
+      [0, 1],
+      [0, 2]
+    ]
+
+    @moves_black = [
+      [0, -1],
+      [0, -2]
+    ]
+
+    @moves_white.each do |move|
+      move[0] += x1
+      move[1] += y1
+    end
+
+    @moves_black.each do |move|
+      move[0] += x1
+      move[1] += y1
+    end
+
+    # first if, checking for initial position to determine valid 2-space move
+    if (color == true && y_position == 2) || (color == false && y_position == 7)
+      # checks if the 2-space move requested works
+      return true if (color == true && @moves_white.include?(destination)) || (color == false && @moves_black.include?(destination))
+    else
+      # if not on its first move, checks that moves are 1-space
+      return true if (color == true && x2 == x1 + 1) || (color == false && x2 == x1 + 1)
+    end
+
+    is_obstructed?([x1, y1], [x2, y2])
+  end
 end
--- spec/models/pawn_spec.rb
+++ spec/models/pawn_spec.rb
@@ -1,4 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe Pawn, type: :model do
+  describe 'valid_move?' do
+    it 'verifies valid moves' do
+      game = FactoryBot.create(:game)
+      game.populate_game
+      piece = Pawn.last
+
+      # checking initial double move
+      piece.valid_move?([8, 7], [8, 5])
+      expect(true)
+
+      # checking double move is not valid anymore by from initial placement
+      piece.valid_move?([8, 6], [8, 4])
+      expect(false)
+
+      # checking for single move
+      piece.valid_move?([8, 7], [8, 6])
+      expect(true)
+
+      # checking for single move starting from other than initial placement
+      piece.valid_move?([8, 6], [8, 5])
+      expect(true)
+
+      # checking for a move that is obstructed
+    end
+  end
 end
