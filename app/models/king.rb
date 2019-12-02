c8a5ebd3b78bd56ffec8d574182018d31084e5af
Robert Currie <currierobertm@gmail.com>
2019-12-02 18:55:49 +0000

    Rubocop

--- app/models/king.rb
+++ app/models/king.rb
@@ -29,17 +29,17 @@ class King < ChessPiece
     return true if @moves.include?([x2, y2])
   end
 
-  def can_castle?(x,y)
-    self.has_moved == false && (castle_kingside?(x,y) || castle_queenside?(x,y))
+  def can_castle?(x_destination, y_destination)
+    self.has_moved == false && (castle_kingside?(x_destination,y_destination) || castle_queenside?(x_destination,y_destination))
     # A king cannot castle if it is in check and cannot castle if castling will put it in check.
   end
 
-  def castle_kingside?(x, y)
-    x == 7 && y = y_position && rook_kingside.has_moved == false && rook_kingside.is_obstructed?([5, y]) == false
+  def castle_kingside?(x_destination, y_destination)
+    x_destination == 7 && y_destination == y_position && rook_kingside.has_moved == false && rook_kingside.is_obstructed?([5, y_destination]) == false
   end
 
-  def castle_queenside?(x,y)
-    x == 2 && y = y_position && rook_queenside.has_moved == false && rook_queenside.is_obstructed?([5, y]) == false
+  def castle_queenside?(x_destination,y_destination)
+    x_destination == 2 && y_destination == y_position && rook_queenside.has_moved == false && rook_queenside.is_obstructed?([5, y_destination]) == false
   end
 
   def rook_kingside
@@ -50,14 +50,14 @@ class King < ChessPiece
     @rook_queenside = ChessPiece.where(x_position: 1, y_position: y_position, color: color, type: 'Rook').first
   end
 
-  def castle!(x, y)
-    if castle_kingside?(x, y)
-      rook_kingside.update_attributes(x_position: 6, y_position: y)
+  def castle!(x_destination, y_destination)
+    if castle_kingside?(x_destination, y_destination)
+      rook_kingside.update_attributes(x_position: 6, y_position: y_destination)
     end
-    if castle_queenside?(x, y)
-      rook_queenside.update_attributes(x_position: 4, y_position: y)
+    if castle_queenside?(x_destination, y_destination)
+      rook_queenside.update_attributes(x_position: 4, y_position: y_destination)
     end
-    update_attributes(x_position: x, y_position: y)
+    update_attributes(x_position: x, y_position: y_destination)
   end
 
   # def move_to!(x, y)
