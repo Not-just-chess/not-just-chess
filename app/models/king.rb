c5edd3b63428d8f6f0cb59ad92c2dc3ce878d6a4
Robert Currie <currierobertm@gmail.com>
2019-12-03 22:24:41 +0000

    Castling works without a button I am so happy

--- app/models/king.rb
+++ app/models/king.rb
@@ -27,29 +27,30 @@ class King < ChessPiece
 
     return false if off_board?(x2, y2) || is_obstructed?(destination)
 
-
     return true if @moves.include?([x2, y2])
-  end
 
-  def can_castle?(x_destination, y_destination)
-    self.has_moved == false && (castle_kingside?(x_destination,y_destination) || castle_queenside?(x_destination,y_destination))
-    # A king cannot castle if it is in check and cannot castle if castling will put it in check.
-  end
-
-  def castle_kingside?(x_destination, y_destination)
-    x_destination == 7 && y_destination == y_position && rook_kingside.has_moved == false && rook_kingside.is_obstructed?([5, y_destination]) == false
-  end
-
-  def castle_queenside?(x_destination,y_destination)
-    x_destination == 2 && y_destination == y_position && rook_queenside.has_moved == false && rook_queenside.is_obstructed?([5, y_destination]) == false
+    return true if can_castle?(x2, y2)
   end
 
   def rook_kingside
-    @rook_kingside = ChessPiece.where(x_position: 8, y_position: y_position, color: color, type: 'Rook').first
+    @rook_kingside = ChessPiece.where(game_id: game_id, x_position: 8, color: color, type: 'Rook').first
   end
 
   def rook_queenside
-    @rook_queenside = ChessPiece.where(x_position: 1, y_position: y_position, color: color, type: 'Rook').first
+    @rook_queenside = ChessPiece.where(game_id: game_id, x_position: 1, color: color, type: 'Rook').first
+  end
+
+  def can_castle?(x_destination, y_destination)
+    has_moved == false && (castle_kingside?(x_destination,y_destination) || castle_queenside?(x_destination,y_destination))
+    # A king cannot castle if it is in check and cannot castle if castling will put it in check or if it is currently in check
+  end
+
+  def castle_kingside?(x_destination, y_destination)
+    return true if x_destination == 7 && y_destination == y_position && rook_kingside.has_moved == false && rook_kingside.is_obstructed?([5, y_destination]) == false
+  end
+
+  def castle_queenside?(x_destination,y_destination)
+    return true if x_destination == 3 && y_destination == y_position && rook_queenside.has_moved == false && rook_queenside.is_obstructed?([5, y_destination]) == false
   end
 
   def castle!(x_destination, y_destination)
@@ -62,8 +63,10 @@ class King < ChessPiece
     update_attributes(x_position: x_destination, y_position: y_destination)
   end
 
-  # def move_to!(x, y)
-  #   castle!(x, y) if can_castle?(x,y)
-  #   super(x,y)
-  # end
+  def move_to!(destination)
+    x2 = destination[0].to_i
+    y2 = destination[1].to_i
+    castle!(x2, y2) if can_castle?(x2,y2)
+    super(destination)
+  end
 end
