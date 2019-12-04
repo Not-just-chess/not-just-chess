64cea43e7912b04d3f17d70715e6dd6174e87063
Bre Phelps <bphelps@live.com>
2019-12-04 18:28:06 +0000

    migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,68 +10,70 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_191_204_031_457) do
+ActiveRecord::Schema.define(version: 2019_12_04_052145) do
+
   # These are extensions that must be enabled in order to support this database
-  enable_extension 'plpgsql'
+  enable_extension "plpgsql"
 
-  create_table 'bishops', force: :cascade do |t|
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
+  create_table "bishops", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-
-  create_table 'chess_pieces', force: :cascade do |t|
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.string 'type'
-    t.integer 'game_id'
-    t.boolean 'color'
-    t.boolean 'captured'
-    t.integer 'x_position'
-    t.integer 'y_position'
-    t.string 'htmlcode'
-    t.boolean 'has_moved', default: false
-    t.index ['game_id'], name: 'index_chess_pieces_on_game_id'
+  create_table "chess_pieces", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "type"
+    t.integer "game_id"
+    t.boolean "color"
+    t.boolean "captured"
+    t.integer "x_position"
+    t.integer "y_position"
+    t.string "htmlcode"
+    t.boolean "has_moved", default: false
+    t.index ["game_id"], name: "index_chess_pieces_on_game_id"
   end
 
-  create_table 'games', force: :cascade do |t|
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.integer 'user_id'
-    t.integer 'white_player_id'
-    t.integer 'black_player_id'
-    t.integer 'winner'
-    t.boolean 'checkmate', default: false
-    t.boolean 'forfeited', default: false
-    t.index ['black_player_id'], name: 'index_games_on_black_player_id'
-    t.index ['user_id'], name: 'index_games_on_user_id'
-    t.index ['white_player_id'], name: 'index_games_on_white_player_id'
-    t.index ['winner'], name: 'index_games_on_winner'
+  create_table "games", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "user_id"
+    t.integer "white_player_id"
+    t.integer "black_player_id"
+    t.integer "winner"
+    t.boolean "checkmate", default: false
+    t.boolean "forfeited", default: false
+    t.integer "turn_player_id"
+    t.index ["black_player_id"], name: "index_games_on_black_player_id"
+    t.index ["turn_player_id"], name: "index_games_on_turn_player_id"
+    t.index ["user_id"], name: "index_games_on_user_id"
+    t.index ["white_player_id"], name: "index_games_on_white_player_id"
+    t.index ["winner"], name: "index_games_on_winner"
   end
 
-  create_table 'kings', force: :cascade do |t|
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
+  create_table "kings", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  create_table 'knights', force: :cascade do |t|
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
+  create_table "knights", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  create_table 'pawns', force: :cascade do |t|
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
+  create_table "pawns", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  create_table 'queens', force: :cascade do |t|
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
+  create_table "queens", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  create_table 'rooks', force: :cascade do |t|
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
+  create_table "rooks", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
