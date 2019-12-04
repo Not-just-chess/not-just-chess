fc2da27f17f9582eed1ca3bab601467cfe420c93
Bre Phelps <bphelps@live.com>
2019-12-04 18:26:57 +0000

    turn logic

--- app/helpers/games_helper.rb
+++ app/helpers/games_helper.rb
@@ -1,12 +1,12 @@
 # Games Helper
 module GamesHelper
-  attr_accessor :pieces, :current_user
+  attr_accessor :pieces, :current_player
 
   def render_piece(game)
     @pieces = ChessPiece.where(game_id: game.id)
   end
 
   def current_user
-    @current_user = current_user
+    @current_player = current_user
   end
 end
