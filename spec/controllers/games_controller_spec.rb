8472b6a99c07841d73faf79e12f81fd2f495fafe
Brian Hong <hong.matchb@gmail.com>
2019-12-04 22:40:31 +0000

    Rspec edits

--- spec/controllers/games_controller_spec.rb
+++ spec/controllers/games_controller_spec.rb
@@ -55,9 +55,8 @@ RSpec.describe GamesController, type: :controller do
 
   describe 'games#show action' do
     it 'should successfully show the page if a game is found' do
-      game = FactoryBot.create(
-        :game
-      )
+      game = FactoryBot.create(:game)
+      game.populate_game
       get :show, params: { id: game.id }
       expect(response).to have_http_status(:success)
     end
