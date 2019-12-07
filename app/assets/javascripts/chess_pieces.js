423fa7fcc4b13f92334d880d0aef56d1bf82bb9b
Ivan Perez <ivan.perez002@gmail.com>
2019-12-07 01:31:09 +0000

    completed pawn_promotion

--- app/assets/javascripts/chess_pieces.js
+++ app/assets/javascripts/chess_pieces.js
@@ -8,9 +8,9 @@ $(document).ready(function() {
   });
 
   function canPromote() {
-    let piece = $(this)
-    if (piece.data('piece-type') === "Pawn" && piece.data('y_position') === "8" || piece.data('y_position') === "1")  
-      return true
+    console.log(this);
+    if (this.type === "Pawn" && this.y_position == 8 || this.y_position == 1)  
+      return true;
     else
       return false;
   };
@@ -24,7 +24,7 @@ $(document).ready(function() {
   var queenBtn = document.getElementById("queenBtn")
 
   // Get the <span> element that closes the modal
-  var span = document.getElementsByClassName("close")[0];
+  var span = document.getElementsByClassName("close");
 
   // When the user clicks on the button, open the modal
   btn.onclick = function() {
@@ -49,40 +49,44 @@ $(document).ready(function() {
 // above here is the promotion modal JS
 
   $('.droppable').droppable({
-      drop: function(event, ui) {
-          let piece = ui.draggable
-          let destination_square = $(this);
-          console.log($(this));
-          var update_piece = {
-              id: piece.data('piece-id'),
-              game_id: piece.data('game-id'),
-              x_position: destination_square.data('x_position'),
-              y_position: destination_square.data('y_position')
-          }
-
-          if (update_piece.canPromote) { 
-              document.getElementById("pawnPromoteModal").style.display = "block"
-              $('modalEx submit').onSubmit(function(e) {
-                  $.ajax({
-                      type: 'PATCH',
-                      url: '/chess_pieces/' + update_piece.id,
-                      beforeSend: function(xhr) { xhr.setRequestHeader('X-CSRF-Token', $('meta[name="csrf-token"]').attr('content')) },
-                      dataType: 'json',
-                      data: {
-                          id: update_piece.id,
-                          game_id: update_piece.game_id,
-                          x_position: update_piece.x_position,
-                          y_position: update_piece.y_position,
-                          type: $('pawnPromoteModal form radio-btn active').value
-                      },
-                      success: function(data) {
-                          destination_square.empty();
-                          location.reload(true);
-                      }
-                  })
-              })
-          }
-
+    drop: function(event, ui) {
+        let piece = ui.draggable
+        let destination_square = $(this);
+        var update_piece = {
+            id: piece.data('piece-id'),
+            game_id: piece.data('game-id'),
+            x_position: destination_square.data('x_position'),
+            y_position: destination_square.data('y_position'),
+            type: piece.data('piece-type'),
+            color: piece.data('color'),
+            canPromote: canPromote
+        }
+        console.log(update_piece)
+        if (update_piece.canPromote()) { 
+          document.getElementById("pawnPromoteModal").style.display = "block"
+          $('#promote_container button').click(function(e) {
+          document.getElementById("pawnPromoteModal").style.display = "none"
+          console.log(chooseCode(update_piece))
+            $.ajax({
+                type: 'PATCH',
+                url: '/chess_pieces/' + update_piece.id,
+                beforeSend: function(xhr) { xhr.setRequestHeader('X-CSRF-Token', $('meta[name="csrf-token"]').attr('content')) },
+                dataType: 'json',
+                data: {
+                    id: update_piece.id,
+                    game_id: update_piece.game_id,
+                    x_position: update_piece.x_position,
+                    y_position: update_piece.y_position,
+                    type: $(e.target).val(),
+                    htmlcode: chooseCode(update_piece.color, $(e.target).val())
+                },
+                success: function(data) {
+                    destination_square.empty();
+                    // location.reload(true);
+                }
+            })
+          })
+        } else {
           $.ajax({
               type: 'PATCH',
               url: '/chess_pieces/' + update_piece.id,
@@ -96,22 +100,46 @@ $(document).ready(function() {
               },
               success: function(data) {
                   destination_square.empty();
-                  location.reload(true);
+                  // location.reload(true);
               }
           });
+        }
       }
   });
+
 });
 
 
-
-
-
-  //def pawn_promotion(destination)
-    //if self.type(Pawn) && y_position == 8 || y_position == 1
-      //launch_modal
-      //update_attributes(type: "#{@promotion_type}", )
-    //end
-  //end
-
-
+function chooseCode(color, type) {
+  if (color === true){ 
+    switch(type) {
+      case 'Queen':
+        return '&#9813;';
+        break;
+      case 'Bishop':
+        return '&#9815;';
+        break;
+      case 'Rook':
+        return '&#9814;';
+        break;
+      case 'Knight':
+        return '&#9816;';
+        break;
+    }
+  } else {
+    switch(type) {
+      case 'Queen':
+        return '&#9819';
+        break;
+      case 'Bishop':
+        return '&#9821;';
+        break;
+      case 'Rook':
+        return '&#9820;';
+        break;
+      case 'Knight':
+        return '&#9822;';
+        break;
+    }
+  }
+}
